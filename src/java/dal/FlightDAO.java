4a16e627190ccfd19ce85be861c65da6eb1eb7da
rina2004 <shinnolove.2004@gmail.com>
2025-02-24 13:03:51 +0700

    Delete SSMS lib

--- src/java/dal/FlightDAO.java
+++ src/java/dal/FlightDAO.java
@@ -266,4 +266,24 @@ public class FlightDAO extends DBContext<Flight> {
         }
         return price;
     }
+
+    @Override
+    public void insert(Flight model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Flight model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Flight model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Flight> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
